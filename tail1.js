bd600bd6dd86656950bee96a2c6cace93c32f1fe
cshijiusheng <40679975+cshijiusheng@users.noreply.github.com>
2018-07-07 09:59:16 +0800

    Create tail1.js

---
+++ tail1.js
@@ -0,0 +1,10 @@
+$(function(){
+	$(".footer").load("tail1.html",function(){	
+		var link=document.createElement("link");
+		link.rel="stylesheet";
+		link.href="css/tail1.css";
+		document.head.appendChild(link);		
+    })   	   
+});  
+	
+		 
