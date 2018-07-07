66baea4c80084a774ce33d7c3771df768c22a722
cshijiusheng <40679975+cshijiusheng@users.noreply.github.com>
2018-07-07 09:50:46 +0800

    Create header1.js

---
+++ header1.js
@@ -0,0 +1,9 @@
+$(function(){
+	$(".logo").load("header1.html",function(){	
+		var link=document.createElement("link");
+		link.rel="stylesheet";
+		link.href="css/header1.css";
+		document.head.appendChild(link);		
+    })   	   
+});  
+	   
